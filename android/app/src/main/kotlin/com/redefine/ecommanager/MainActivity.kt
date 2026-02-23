ff3180b66d6378cf4cced91c90dc5c0dd6e89dde
Sameer <pathansameerahmed@gmail.com>
2026-02-23 15:11:53 +0530

    UI/UX change

--- android/app/src/main/kotlin/com/redefine/ecommanager/MainActivity.kt
+++ android/app/src/main/kotlin/com/redefine/ecommanager/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.redefine.ecommanager
 
 import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity : FlutterFragmentActivity()
